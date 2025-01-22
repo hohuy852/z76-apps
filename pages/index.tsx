a835413ac72f2c2e1afc82a846c401ca15c1cfcd
hohuy852 <57467992+hohuy852@users.noreply.github.com>
2025-01-23 00:54:38 +0800

    updae

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,62 +3,67 @@ import { HotTable, HotColumn } from "@handsontable/react-wrapper";
 import { registerAllModules } from "handsontable/registry";
 import "handsontable/styles/handsontable.css";
 import "handsontable/styles/ht-theme-main.css";
-
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import HourglassBottomIcon from "@mui/icons-material/HourglassBottom";
 // register Handsontable's modules
 registerAllModules();
-import data from "../data/data.json"
+import data from "../data/data.json";
 import { Grid2 } from "@mui/material";
+import CustomCard from "@/components/Card";
+import CustomChart from "@/components/Chart";
 export default function HomePage() {
   return (
-    <Grid2 container height="100%" spacing={2}>
-      <Grid2 size={12}>
+    <div>
+      <Grid2 container width="100%" spacing={2}>
+        <Grid2 size={3}>
+          <CustomCard
+            subtitle="Hồ sơ đã duyệt"
+            value={100}
+            title=""
+            additionalInfo="7 ngày qua"
+            IconComponent={AssignmentTurnedInIcon}
+          ></CustomCard>
+        </Grid2>
+        <Grid2 size={3}>
+          <CustomCard
+            subtitle="Hồ sơ chờ duyệt"
+            value={100}
+            title=""
+            additionalInfo="7 ngày qua"
+            IconComponent={HourglassBottomIcon}
+          ></CustomCard>
+        </Grid2>
+        <Grid2 size={3}>
+          <CustomCard
+            subtitle="Hồ sơ đã duyệt"
+            value={100}
+            title=""
+            additionalInfo="7 ngày qua"
+            IconComponent={AssignmentTurnedInIcon}
+          ></CustomCard>
+        </Grid2>
+        <Grid2 size={3}>
+          <CustomCard
+            subtitle="Hồ sơ đã duyệt"
+            value={100}
+            title=""
+            additionalInfo="7 ngày qua"
+            IconComponent={AssignmentTurnedInIcon}
+          ></CustomCard>
+        </Grid2>
       </Grid2>
-      <Grid2 size={12}>
-        <HotTable
-          data={data.data}
-          autoColumnSize
-          height="100%"
-          width="100%"
-          colHeaders={data.headers}
-          contextMenu={[
-            "cut",
-            "copy",
-            "---------",
-            "row_above",
-            "row_below",
-            "remove_row",
-            "---------",
-            "alignment",
-            "make_read_only",
-            "clear_column",
-          ]}
-          dropdownMenu={true}
-          hiddenColumns={{
-            indicators: true,
-          }}
-          multiColumnSorting={true}
-          filters={true}
-          rowHeaders={true}
-          headerClassName="htLeft"
-          manualRowMove={true}
-          autoWrapRow={true}
-          autoWrapCol={true}
-          manualRowResize={true}
-          manualColumnResize={true}
-          navigableHeaders={true}
-          licenseKey="non-commercial-and-evaluation"
-          className="ht-theme-main" 
-        >
-          <HotColumn data={1} />
-          <HotColumn data={3} />
-          <HotColumn data={4} type="date" allowInvalid={false} />
-          <HotColumn data={6} type="checkbox" className="htCenter" />
-          <HotColumn data={7} type="numeric" />
-          <HotColumn data={5} />
-          <HotColumn data={2} />
-        </HotTable>
+      <Grid2 container width="100%" marginTop={3} size={12}>
+        <CustomChart></CustomChart>
       </Grid2>
-    </Grid2>
+      <Grid2 container width="100%" marginTop={3} size={12}>
+        <Grid2 size={6}>
+
+        </Grid2>
+        <Grid2 size={6}>
+          
+        </Grid2>
+      </Grid2>
+    </div>
   );
 }
 
